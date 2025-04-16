3b62c4f20e02a802c8682a3da573122fd47b1298
GlitchAsylum <cyberpunkhero@protonmail.com>
2025-04-16 14:08:21 -0500

    Center Content

--- app/page.tsx
+++ app/page.tsx
@@ -5,18 +5,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className=" items-center justify-items-center min-h-screen">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="flex justify-center items-center min-h-screen">
+      <main className="flex flex-col gap-[32px] items-center text-center">
         <Image
-          src="/whowe_logo.svg"
+          src="/whowe_logo_noText.svg"
           alt="Whowe logo"
           width={180}
           height={40}
           priority
         />
-        <h1 className="mb-2 text-4xl font-bold leading-tight tracking-[.05em]">Everyone has a story</h1>
-        <h3 className="mb-2 text-3xl font-bold leading-tight tracking-[.05em]">Whats yours?</h3>
-        <div className="sm:text-left w-112 mb-2">
+        <h1 className="text-7xl font-bold leading-tight tracking-[.05em]">Whowe</h1>
+        <h1 className="text-4xl font-bold leading-tight tracking-[.05em]">Everyone has a story.</h1>
+        <div className="sm:text-left w-112 text-center">
           Whowe is a revolutionary social history platform designed to help you record, share, and even sell your life stories and traditions to current and future generations.{" "}
         </div>               
       </main>
