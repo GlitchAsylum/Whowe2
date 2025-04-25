67ecc10a4a35bd869d597ea60952ea8fc25b9791
GlitchAsylum <cyberpunkhero@protonmail.com>
2025-04-24 20:40:45 -0500

    page tweak

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import Image from 'next/image';
+import Link from 'next/link'
 import Button from '@/app/ui/components/buttons/Button';
 import { useCallback } from 'react';
 
@@ -38,7 +39,7 @@ export default function Home() {
           style={{ objectFit: 'contain' }}
         />
         <h1 className="text-4xl font-bold tracking-[0.05em] md:text-5xl">{heroContent.title}</h1>
-        <h3 className="text-xl font-semibold tracking-[0.05em] md:text-2xl">
+        <h3 className="text-xl md:text-l font-semibold tracking-[0.05em] md:text-2xl">
           {heroContent.subtitle}
         </h3>
         <p className="mx-auto max-w-2xl text-base leading-relaxed tracking-[0.05em] text-gray-300 sm:text-lg">
@@ -53,6 +54,17 @@ export default function Home() {
         >
           {heroContent.buttonText}
         </Button>
+        <Link href="/about">
+            <Button
+              onClick={handleClick}
+              variant="glass"
+              size="large"
+              className="w-full"
+              aria-label="Learn more about us"
+            >
+              About Us
+            </Button>
+          </Link>
       </main>
     </div>
   );
